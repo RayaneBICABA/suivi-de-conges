018209dd50db04a386cdd65299ea9cfffade9578
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-08-18 12:16:18 +0000

    feat(domain): The model Conge base code is written using JPA Specification for database and Lombok for auto-generated Getters, Setters and constructors

--- backend/src/main/java/com/ravex/backend/domain/model/Agent.java
+++ backend/src/main/java/com/ravex/backend/domain/model/Agent.java
@@ -1,5 +1,44 @@
 package com.ravex.backend.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+// Lombok : @Data = Getters + Setters + toString + equals + hashCode
+// @NoArgsConstructor = constructeur vide (JPA l’exige)
+// @AllArgsConstructor = constructeur complet
+// @Builder = facilite la création d’objets avec un pattern builder
+
+
+@Entity
+@Table(name = "AGENT")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Agent {
-    
+    // =========== Initialisation des Attributs ===========
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "A_MATRICULE", nullable = false, length = 6)
+    private String matricule;
+
+    @Column(name = "A_NOM", length = 75)
+    private String nom;
+
+    @Column(name = "A_PRENOM", length = 30)
+    private String prenom;
+
+    @Column(name = "A_UNITE", length = 30)
+    private String unite;
+
+    @Column(name = "A_SERVICE", length = 30)
+    private String service;
+
+    @Column(name = "A_FONCTION", length = 50)
+    private String fonction;
+
+    // =========== Accesseur Personnalisé===========
+    public String getFullName(){
+        return nom+" "+prenom;
+    }
 }
--- backend/src/main/java/com/ravex/backend/domain/model/Conge.java
+++ backend/src/main/java/com/ravex/backend/domain/model/Conge.java
@@ -1,5 +1,27 @@
 package com.ravex.backend.domain.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "CONGE")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Conge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CO_REF", length = 15, nullable = false)
+    private String reference;
+
     
+    @Column(name = "CO_JOUR", nullable = false)
+    private Integer jours;
+
+    @Column(name = "CO_DATE")
+    private LocalDate dateDemande;
 }
