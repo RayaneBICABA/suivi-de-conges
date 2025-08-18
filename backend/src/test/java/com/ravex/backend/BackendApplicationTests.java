f212480e130b546e140815f02b01a66269812eb9
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-08-18 10:43:36 +0000

    feat(backend): Base REST Full API code init, and connection to the distant Oracle database init

---
+++ backend/src/test/java/com/ravex/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
