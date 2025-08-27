7a62eed49cec0241435001bcd34f11c352add5a2
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-08-27 15:21:32 +0000

    ======STAYING ERROR HANDLING ON REMOTE USER MACHINE=====

--- backend/src/main/java/com/ravex/backend/configuration/CorsConfig.java
+++ backend/src/main/java/com/ravex/backend/configuration/CorsConfig.java
@@ -12,9 +12,40 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173") // ton frontend
+                // Autorise toutes les origines (tous les PC du réseau local)
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // ⚠️ uniquement pour dev/test
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
 }
+
+
+
+
+
+
+
+// package com.ravex.backend.configuration;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration
+// public class CorsConfig {
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173") // ton frontend
+//                         .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                         .allowedOrigins("*"); // ⚠️ seulement pour dev/test, pas en prod
+
+//             }
+//         };
+//     }
+// }
