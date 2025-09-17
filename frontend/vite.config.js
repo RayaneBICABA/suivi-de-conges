dca2dddbe1d7d345a3e170a2392b47ec1bd1ec80
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-09-17 03:24:43 +0000

    Update vite.config.js - configuration build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'index.html'),
         login: path.resolve(__dirname, 'src/html/login.html'),
-        register: path.resolve(__dirname, 'src/html/register.html')
+        register: path.resolve(__dirname, 'src/html/register.html'),
+        addAgent: path.resolve(__dirname, 'src/html/ajouterAgent.html'),
+        gererConge: path.resolve(__dirname, 'src/html/gererConges.html'),
       }
     }
   }
