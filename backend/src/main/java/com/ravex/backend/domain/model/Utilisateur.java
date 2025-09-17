fce91ce0c796e4e8a270486394fda853db0b0f21
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-09-17 03:24:43 +0000

    Mise à jour modèle Utilisateur - sécurité

--- backend/src/main/java/com/ravex/backend/domain/model/Utilisateur.java
+++ backend/src/main/java/com/ravex/backend/domain/model/Utilisateur.java
@@ -10,8 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Utilisateur {
-    
-    // Clé primaire - Changement principal ici
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_utilisateur")
     @SequenceGenerator(
@@ -21,36 +20,28 @@ public class Utilisateur {
     )
     @Column(name = "user_id")
     private Long userId;
-    
-    // Nom d'utilisateur
+
     @Column(name = "username", length = 50, nullable = false)
     private String username;
-    
-    // Nom
+
     @Column(name = "lastname", length = 100, nullable = false)
     private String lastname;
-    
-    // Prenom
+
     @Column(name = "firstname", length = 100, nullable = false)
     private String firstname;
-    
-    // Mot de passe
+
     @Column(name = "password", nullable = false)
     private String password;
-    
-    // Email
+
     @Column(name = "email", nullable = false, unique = true)
     private String email;
-    
-    // Enable / Disable
+
     @Column(name = "enabled")
     private boolean enabled = true;
     
     @Column(name = "role", nullable = false)
     private String role = "USER";
 
-
-    // ========= RELATION ENTRE DIRECTION ET UTILISATEUR  =========
     @ManyToOne
     @JsonBackReference
     @JoinColumn(name = "NUM_DR")
