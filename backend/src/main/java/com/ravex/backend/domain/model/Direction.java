5c89b33241dfd2cb73f2e90b401229bc10487ebd
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-09-17 03:24:44 +0000

    Update modèle Direction - structure

--- backend/src/main/java/com/ravex/backend/domain/model/Direction.java
+++ backend/src/main/java/com/ravex/backend/domain/model/Direction.java
@@ -19,13 +19,15 @@ public class Direction {
     @Column(name = "NOM_DR", length = 6)
     private String nom;
 
-    // ========= RELATION ENTRE DIRECTION ET CENTRE  =========
     @OneToMany(mappedBy = "direction")
     @JsonIgnore
     private List<Centre> centres;
 
-    // ========= RELATION ENTRE DIRECTION ET UTILISATEUR  =========
     @OneToMany(mappedBy = "direction")
     @JsonIgnore
     private List<Utilisateur> utilisateurs;
+
+    @OneToMany(mappedBy = "direction")
+    @JsonIgnore
+    public List<Agent> agents;
 }
