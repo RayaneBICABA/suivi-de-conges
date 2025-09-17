15de0182c9c3ac3892f34cc15bc9307d9d9240e1
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-09-17 03:24:43 +0000

    Update SuiviCongeRepository - nouvelles fonctionnalités

--- backend/src/main/java/com/ravex/backend/domain/Repository/SuiviCongeRepository.java
+++ backend/src/main/java/com/ravex/backend/domain/Repository/SuiviCongeRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface SuiviCongeRepository extends JpaRepository<SuiviConge, Long> {
-    // Obtenir la somme des jours de congés épuisés (Somme des SC_JOUR) pour un agent donné
     @Query("SELECT COALESCE(SUM(s.jours), 0) FROM SuiviConge s WHERE s.conge.reference = :reference")
     Integer totaljoursEpuise(@Param("reference") String reference);
 
