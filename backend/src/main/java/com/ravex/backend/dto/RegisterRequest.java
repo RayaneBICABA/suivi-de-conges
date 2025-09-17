d3ed20b1bb90674385fa8048927478661b2b7300
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-09-17 03:24:44 +0000

    Refactoring RegisterRequest - champs requis

--- backend/src/main/java/com/ravex/backend/dto/RegisterRequest.java
+++ backend/src/main/java/com/ravex/backend/dto/RegisterRequest.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
-// Requête d'inscription
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
